96e67cb0c685f70d8662db5da051d5be69ad8de3
Bharat khandelwal <b0143k@gmail.com>
2023-05-31 21:30:34 +0530

    tweet-icons

--- src/components/pages/Icons.jsx
+++ src/components/pages/Icons.jsx
@@ -1,37 +1,44 @@
 import React, { useState } from 'react'
 import style from './Icons.module.css';
-import { BiHeart,BiRepost,BiComment } from "react-icons/bi";
+import { BiHeart, BiRepost, BiComment, BiBarChart } from "react-icons/bi";
+import { FiShare } from "react-icons/fi";
 
 
 function Icons() {
-    const[heartCount, setHeartCount]=useState(0);
-    const[repostCount, setRepostCount]=useState(0);
-    const[commentCount, setCommentCount]=useState(0);
-   function onHeartClick(){
-setHeartCount(heartCount+1)
+    const [heartCount, setHeartCount] = useState(0);
+    const [repostCount, setRepostCount] = useState(0);
+    const [commentCount, setCommentCount] = useState(237);
+    function onHeartClick() {
+        setHeartCount(heartCount + 1)
     }
-    function onRepostClick(){
-        setRepostCount(repostCount+1);
+    function onRepostClick() {
+        setRepostCount(repostCount + 1);
     }
-    function onCommentClick(){
-        setCommentCount(commentCount+1);
+    function onCommentClick() {
+        setCommentCount(commentCount + 1);
     }
-  return (
-    <div>
-      <div className={style.postStastics}>
-        <div onClick={onHeartClick} className={style.heart} title='Like'>
-          <BiHeart  /> {heartCount}
+    return (
+        <div>
+            <div className={style.postStastics}>
+                <div onClick={onHeartClick} className={style.heart} title='Like'>
+                    <BiHeart /> {heartCount}
+                </div>
+                <div onClick={onRepostClick} className={style.repost} title='Repost'>
+                    <BiRepost /> {repostCount}
+                </div>
+                <div onClick={onCommentClick} className={style.comment} title='Comment'>
+                    <BiComment /> {commentCount}
+                </div>
+                <div onClick={() => { }} className={style.graph} title='Graph'>
+                    < BiBarChart /> {3.7}k
+                </div>
+                <div onClick={() => { }} className={style.share} title='Share'>
+                    < FiShare /> 
+                </div>
+            </div>
+
         </div>
-        <div onClick={onRepostClick} className={style.repost} title='Repost'>
-          <BiRepost  /> {repostCount}
-        </div>
-        <div onClick={onCommentClick} className={style.comment} title='Comment'>
-          <BiComment  /> {commentCount}
-        </div>
-    
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Icons;
